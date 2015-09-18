65ca3cfce097d340d5add321d2c53055fe6eb8ca
qwemicheal <qwemicheal@gmail.com>
2015-09-18 15:06:39 +0000

    use code from the tutorial contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,19 @@
+#class ContactMailer < ActionMailer::Base
+  #  default to: 'qwemicheal@gmail.com'    
+  #  def contact_email(name,email,body)
+  #      @name=name
+ #       @email=email
+  #      @body=body
+#        mail(from: email,subject:'contact from message')
+ #   end
+#end
+
 class ContactMailer < ActionMailer::Base
-    default to: 'qwemicheal@gmail.com'    
-    def contact_email(name,email,body)
-        @name=name
-        @email=email
-        @body=body
-        mail(from: email,subject:'contact from message')
-    end
+  default to: 'your@email_address.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
